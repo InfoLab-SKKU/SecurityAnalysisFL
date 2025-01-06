43c13a558d436972c3c47864dd3f127d01a2389d
abdenour soubih <soubih.abdenour@yahoo.com>
2025-01-06 12:12:45 +0900

    Update configs and scripts for LDP and resource adjustments
    Reduced client count and updated CPU/GPU allocations in configs to optimize resource usage. Adjusted local differential privacy (LDP) configurations, including support for epsilon in experiment scripts. Expanded experiment runs with varying LDP epsilon values.

--- scripts/multirun.sh
+++ scripts/multirun.sh
@@ -12,17 +12,18 @@ run_experiment() {
   local partition_param=$5
   local partition_value=$6
   local num_classes=$7
+  local epsilon=$8
 
   # Generate timestamp for directory
   timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
   # Define output directory path with timestamp
-  output_dir="outputs/${strategy}/${model}/${dataset}/${partitioner}/${partition_value}/${timestamp}"
+  output_dir="outputs/${strategy}/${model}/${dataset}/${partitioner}/${partition_value}/ldp_${epsilon}/${timestamp}"
 
   echo "Starting experiment with dataset=${dataset}, partitioner=${partitioner}, ${partition_param}=${partition_value}, model.num_classes=${num_classes}"
 
   # Run the experiment
-  python -m src.main hydra.run.dir=$output_dir dataset.subset="$dataset" strategy.name="$strategy" model.name="$model" dataset.partitioner.name="$partitioner" dataset.partitioner.$partition_param="$partition_value" model.num_classes="$num_classes"
+  python -m src.main hydra.run.dir=$output_dir dataset.subset="$dataset" strategy.name="$strategy" model.name="$model" dataset.partitioner.name="$partitioner" dataset.partitioner.$partition_param="$partition_value" model.num_classes="$num_classes" ldp.epsilon="$epsilon"
 
   # Check if the experiment failed
   if [ $? -ne 0 ]; then
@@ -54,14 +55,20 @@ run_experiment() {
 #run_experiment "FedAvg" "mobilenet_v2" "tissuemnist" "DirichletPartitioner" "alpha" 0.1 8
 
 ## Run experiments for bloodmnist with PathologicalPartitioner
-#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "PathologicalPartitioner" "num_classes_per_partition" 2 8
-#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "PathologicalPartitioner" "num_classes_per_partition" 4 8
-#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "PathologicalPartitioner" "num_classes_per_partition" 7 8
+#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "PathologicalPartitioner" "num_classes_per_partition" 2 8 0.3
+#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "PathologicalPartitioner" "num_classes_per_partition" 4 8 0.7
+#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "PathologicalPartitioner" "num_classes_per_partition" 7 8 0
 
 ## Run experiments for bloodmnist with DirichletPartitioner
-#run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.9 8
-#run_experiment "FedAvg" "mobilenet_v2" "dermamnist" "DirichletPartitioner" "alpha" 0.3 8
-run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "IiD" "alpha" 0.9 8
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.9 8 0
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.9 8 0.7
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.9 8 0.3
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.3 8 0
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.3 8 0.7
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.3 8 0.3
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.1 8 0
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.1 8 0.7
+run_experiment "FedAvg" "mobilenet_v2" "bloodmnist" "DirichletPartitioner" "alpha" 0.1 8 0.3
 
 # Final report
 echo "Experiments completed!"
