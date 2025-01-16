43b01a7d2732a97ecd5800a77d01cfd685aeefb3
abdenour soubih <soubih.abdenour@yahoo.com>
2025-01-17 00:16:08 +0900

    Refactor attack module with base class and modular structure
    Introduced an abstract base class `Attack` and refactored sub-classes like `AutoRegressorAttack` to inherit from it. Added placeholder classes `Benin` and `Noops` for future extensions. Streamlined convolution operation and adjusted parameter handling for clarity and functionality.

--- src/modules/attacks/attack.py
+++ src/modules/attacks/attack.py
@@ -6,8 +6,45 @@ import numpy as np
 from src.modules.attacks.utils import get_ar_params
 
 
+class Attack(ABC):
+    """
+    A generic interface for attack
+    """
 
-class AutoRegressorAttack:
+    def on_batch_selection(self, inputs: torch.Tensor, targets: torch.Tensor):
+        
+        return inputs, targets
+
+    def on_before_backprop(self, model, loss):
+        return model, loss
+
+    def on_after_backprop(self, model, loss):
+        return model, loss
+
+class Benin(ABC):
+    def on_batch_selection(self, inputs: torch.Tensor, targets: torch.Tensor):
+        return inputs, targets
+
+    def on_before_backprop(self, model, loss):
+        return model, loss
+
+    def on_after_backprop(self, model, loss):
+        return model, loss
+
+
+
+class Noops(ABC):
+    def on_batch_selection(self, inputs: torch.Tensor, targets: torch.Tensor):
+        return inputs, targets
+
+    def on_before_backprop(self, model, loss):
+        return model, loss
+
+    def on_after_backprop(self, model, loss):
+        return model, loss
+
+
+class AutoRegressorAttack(Attack):
 
     def __init__(self, config):
         super().__init__()
@@ -35,10 +72,7 @@ class AutoRegressorAttack:
             self.epsilon = 8/255
 
         self.ar_params = get_ar_params(num_classes=self.num_classes)
-        self.ar_params = [torch.clamp(param, -1, 1) for param in self.ar_params]
-        self.ar_params *= 255
         print(self.crop, self.size, self.gaussian_noise,self.epsilon,self.num_channels,self.num_classes)
-
     def generate(self, index, p=np.inf):
         start_signal = torch.randn((self.num_channels, self.size[0], self.size[1]))
         kernel_size = 3
@@ -49,15 +83,16 @@ class AutoRegressorAttack:
 
         for i in range(rows_to_update):
             for j in range(cols_to_update):
-                val = torch.nn.functional.conv2d(
+                val = F.conv2d(
                     start_signal[:, i: i + kernel_size, j: j + kernel_size],
                     ar_coeff,
                     groups=self.num_channels,
-                )#.clamp(-1,1)
+                )
                 noise = torch.randn(1) if self.gaussian_noise else 0
                 start_signal[:, i + kernel_size - 1, j + kernel_size - 1] = (
                         val.squeeze() + noise
                 )
+
         start_signal_crop = start_signal[:, self.crop:, self.crop:]
         generated_norm = torch.norm(start_signal_crop, p=p, dim=(0, 1, 2))
         scale = (1 / generated_norm) * self.epsilon
@@ -70,7 +105,8 @@ class AutoRegressorAttack:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         for i in range(batch_size):
-            delta, _ = self.generate(p=0.75, index=targets[i])
+            delta, _ = self.generate(p=2, index=targets[i])
+            print(f"iteration {i} -- delta-size={delta.size()}  -- inputs-size={inputs[i].size()}----------------------------------")
             adv_input = (inputs[i] + delta.to(device)).clamp(0, 1)
             adv_inputs.append(adv_input)
         return torch.stack(adv_inputs), targets
@@ -163,7 +199,7 @@ class TargetedLabelFlipAttack(Attack):
 class AttackFactory:
 
     @staticmethod
-    def create_attack(config):
+    def create_attack(config) -> Attack:
         name = config.poisoning.name
         type = config.poisoning.attack_type
         if name == "ar":
